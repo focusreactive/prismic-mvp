1a1179a0321534035046c035744932f66c2f6d42
Eugene Boruhov <eugene@focusreactive.com>
2025-04-04 11:07:01 +0200

    Add token check to `revalidate` route

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -5,7 +5,13 @@ import { revalidateTag } from "next/cache";
  * This endpoint purges Prismic content from Next.js' cache. It is called when
  * content is published in Prismic.
  */
-export async function POST() {
+export async function POST(request: Request) {
+  const {secret} = await request.json();
+
+  if (secret !== process.env.REVALIDATE_TOKEN) {
+    return NextResponse.json({ error: "Invalid secret" }, { status: 401 });
+  }
+
   revalidateTag("prismic");
 
   return NextResponse.json({ revalidated: true, now: Date.now() });
